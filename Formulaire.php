c554b992f3e25ad5f4432d99765cb87743c44212
danieldarosa <daniel.darosa97@gmail.com>
2015-09-02 13:15:24 +0200

    Ajout du type for dans les labels

--- Formulaire.php
+++ Formulaire.php
@@ -1,5 +1,6 @@
-<?php ?>
+
 <!DOCTYPE HTML>
+<?php ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
     <head> 
         <meta http-equiv="content-type" content="test/html; charset=UTF-8" />
@@ -14,35 +15,29 @@
                         <legend>
                             Formulaire d'inscription
                         </legend>
-                        <p>Nom :</p>
+                        
+                        <label for="Nom">Nom :</label>
+                        <input id="nom" type="text" name="nom" id="nom" placeholder="Nom" required autofocus />
 
-                        <p><input type="text" name="nom" id="nom" placeholder="Nom" required autofocus /></p>
+                        <label for="Prénom">Prénom :</label>
+                        <input id="prenom" type="text" name="prenom" id="prenom" placeholder="Prénom" required autofocus />
 
-                        <p>Prénom :</p>
+                        <label for="Pseudo">Pseudonyme :</label>
+                        <input id="pseudo" type="text" name="pseudo" id="pseudo" placeholder="Pseudonyme" required autofocus />
 
-                        <p><input type="text" name="prenom" id="prenom" placeholder="Prénom" required autofocus /></p>
+                        <label for="Email">Email :</label>
+                        <input id="email" type="email"  name="email" id="email" placeholder="email@mondomaine.com" required autofocus/>
 
-                        <p>Pseudonyme :</p>
+                        <label for="Password">Mot de passe :</label>
+                        <input id="password" type="password" name="password" id="password" placeholder="" required autofocus/>
 
-                        <p><input type="text" name="pseudo" id="pseudo" placeholder="Pseudonyme" required autofocus /></p>
+                        <label for="DateNaissance">Date de naissance :</label>
+                        <input id="dateNaissance" type="date" name="date" id="age" placeholder="Votre âge"/>
 
-                        <p>Email :</p>
+                        <label for="Description">Description :</label>
+                        <textarea id="description" name="description" cols="50" rows="10"><?php if (isset($_POST['description'])) echo htmlentities(trim($_POST['description'])); ?></textarea>
 
-                        <p><input type="email"  name="email" id="email" placeholder="email@mondomaine.com" required autofocus/></p>
-
-                        <p>Mot de passe :</p>
-
-                        <p><input type="password" name="password" id="password" placeholder="" required autofocus/></p>
-
-                        <p>Date de naissance :</p>
-
-                        <p><input type="date" name="date" id="age" placeholder="Votre âge"/></p>
-
-                        <p>Description :</p>
-
-                        <p><textarea name="description" cols="50" rows="10"><?php if (isset($_POST['description'])) echo htmlentities(trim($_POST['description'])); ?></textarea></p>
-
-                        <p><input type="submit" id="button" value="Envoyez"   /></p>
+                        <input id="envoyer" type="submit" id="button" value="Envoyez"   />
                     </fieldset>
                 </form>
             </div>
