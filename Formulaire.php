15c79b8297b6e1887ff4df31e78068eec0c6cb51
danieldarosa <daniel.darosa97@gmail.com>
2015-09-02 16:04:56 +0200

    Création de la fonction d'insertion et correction des erreurs PHP + HTML

--- Formulaire.php
+++ Formulaire.php
@@ -1,10 +1,9 @@
 
 <!DOCTYPE HTML>
-<?php ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
     <head> 
-        <meta http-equiv="content-type" content="test/html; charset=UTF-8" />
-        <title></title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <title>Formulaire</title>
         <link href="./cssProjet.css" rel="stylesheet" type="text/css" media="all" charset="UTF-8"></link>
     </head>
     <body>
@@ -15,29 +14,29 @@
                         <legend>
                             Formulaire d'inscription
                         </legend>
-                        
-                        <label for="Nom">Nom :</label>
+
+                        <label for="nom">Nom :</label>
                         <input id="nom" type="text" name="nom" id="nom" placeholder="Nom" required autofocus /><br/>
 
-                        <label for="Prénom">Prénom :</label>
-                        <input id="prenom" type="text" name="prenom" id="prenom" placeholder="Prénom" required autofocus /><br/>
+                        <label for="prenom">Prénom :</label>
+                        <input id="prenom" type="text" name="prenom" id="prenom" placeholder="Prénom" required/><br/>
 
-                        <label for="Pseudo">Pseudonyme :</label>
-                        <input id="pseudo" type="text" name="pseudo" id="pseudo" placeholder="Pseudonyme" required autofocus /><br/>
+                        <label for="pseudo">Pseudonyme :</label>
+                        <input id="pseudo" type="text" name="pseudo" id="pseudo" placeholder="Pseudonyme" required/><br/>
 
-                        <label for="Email">Email :</label>
-                        <input id="email" type="email"  name="email" id="email" placeholder="email@mondomaine.com" required autofocus/><br/>
+                        <label for="email">Email :</label>
+                        <input id="email" type="email"  name="email" id="email" placeholder="email@mondomaine.com" required/><br/>
 
-                        <label for="Password">Mot de passe :</label>
-                        <input id="password" type="password" name="password" id="password" placeholder="" required autofocus/><br/>
+                        <label for="password">Mot de passe :</label>
+                        <input id="password" type="password" name="password" id="password" placeholder="" required/><br/>
 
-                        <label for="DateNaissance">Date de naissance :</label>
+                        <label for="dateNaissance">Date de naissance :</label>
                         <input id="dateNaissance" type="date" name="date" id="age" placeholder="Votre âge"/><br/>
 
-                        <label for="Description">Description :</label>
+                        <label for="description">Description :</label>
                         <textarea id="description" name="description" cols="50" rows="10"><?php if (isset($_POST['description'])) echo htmlentities(trim($_POST['description'])); ?></textarea><br/>
 
-                        <input id="envoyer" type="submit" id="button" value="Envoyez"   />
+                        <input id="envoyer" type="submit" name="envoyer" id="button" value="Envoyez"   />
                     </fieldset>
                 </form>
             </div>
--- Formulaire_insertion.php
+++ Formulaire_insertion.php
@@ -3,51 +3,58 @@ DEFINE("HOST", "127.0.0.1");
 DEFINE("DBNAME", "m151_formulaire");
 DEFINE("USERNAME", "root");
 DEFINE("PASSWORD", "");
-function GetConnection()
-{
-	static $dbh = null;
-	try {
-		if($dbh == null){
-			$dbh = new PDO('mysql:host='.HOST.';dbname='.DBNAME,USERNAME,PASSWORD);
-			}
-	} catch (PDOException $e) {
-		die('Erreur : ' .$e->getMessage());
-	}
-	return $dbh;
+
+function GetConnection() {
+    static $dbh = null;
+    try {
+        if ($dbh == null) {
+            $dbh = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USERNAME, PASSWORD);
+        }
+    } catch (PDOException $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+    return $dbh;
 }
 
-$dbh = GetConnection();
+function InsertUser() {
 
-/*if (!empty($_POST['nom']) && (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) !== false)) {
-	
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $pseudo = $_POST['pseudo'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $date = $_POST['date'];
-    $description = $_POST['description'];
+    $dbh = GetConnection();
 
-    //On prépare la requête d'ajout des données dans la base avec les paramètres choisis
-    $count = $dbh->prepare("INSERT INTO user VALUES('', :nom, :prenom, :pseudo, :email, :password, :date, :description)");
+    if (!empty($_POST['nom'])) {
 
-    //On met en paramètre ce qu'on veut ajouter dans la base
-    $count->bindParam(':nom', $nom, PDO::PARAM_STR);
-    $count->bindParam(':prenom', $prenom, PDO::PARAM_STR);
-    $count->bindParam(':pseudo', $prenom, PDO::PARAM_STR);
-    $count->bindParam(':email', $email, PDO::PARAM_STR);
-    $count->bindParam(':password', $password, PDO::PARAM_STR);
-    $count->bindParam(':date', $date, PDO::PARAM_INT);
-    $count->bindParam(':description', $description, PDO::PARAM_STR);
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $pseudo = $_POST['pseudo'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $date = $_POST['date'];
+        $description = $_POST['description'];
 
-    //On execute la requête
-    $count->execute();
-    
-    header('Location: ./Formulaire.php');
-} else {
-    //Si non, on affiche un message d'erreur
-    echo 'Les champs remplis ne sont pas corrects...';
-}*/
+        //On prépare la requête d'ajout des données dans la base avec les paramètres choisis
+        //INSERT INTO `user`(`nom`, `prénom`, `pseudo`, `email`, `password`, `dateNaissance`, `description`) VALUES (,'test','test','test','test','test','1989-10-10','test')
+        $count = $dbh->prepare("INSERT INTO user(nom,prenom,pseudo,email,password,dateNaissance,description) VALUES(:nom, :prenom, :pseudo, :email, :password, :date, :description)");
+
+        //On met en paramètre ce qu'on veut ajouter dans la base
+        $count->bindParam(':nom', $nom, PDO::PARAM_STR);
+        $count->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+        $count->bindParam(':pseudo', $prenom, PDO::PARAM_STR);
+        $count->bindParam(':email', $email, PDO::PARAM_STR);
+        $count->bindParam(':password', $password, PDO::PARAM_STR);
+        $count->bindParam(':date', $date, PDO::PARAM_INT);
+        $count->bindParam(':description', $description, PDO::PARAM_STR);
+
+        //On execute la requête
+        $count->execute();
+    } else {
+        //Si non, on affiche un message d'erreur
+        echo 'Les champs remplis ne sont pas corrects...';
+    }
+}
+var_dump($_POST);
+if(isset($_POST['envoyer']))
+{
+    InsertUser();
+}
 ?>
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
@@ -59,17 +66,9 @@ $dbh = GetConnection();
 
     <body>
         <div id="Conteneur">
-            <div id="titre">
-                <img src="Images/Titre.png" />
+            <div class="threadMenu">
+                <p></p>
             </div>
-            <div class="menu">
-                <p>Bravo ! Vous faites désormais membre de ce site! Cliquez sur le lien du dessous pour retourner à la page d'acceuil pour pouvoir vous connecter !</p>
-                <a href="./Index.php">Retour à la page d'acceuil</a>
-            </div>
-            <div class="copyright">
-                <img src="Images/Copyright.png" />
-            </div>
-        </div>
     </body>
 </html>
 
